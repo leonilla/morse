dd34ca1120a3bd8517e2fa2de646e85ea0ef427c
Leonardo Bonilla <leanboe@gmail.com>
2023-01-29 22:12:08 +0100

    Menu corrections

--- src/main.c
+++ src/main.c
@@ -178,6 +178,7 @@ int main_menu()
                         printf("Launching interactive mode...\n\n");
                         printf("Type 'exit()' in order to leave interactive mode.\n");
                         launch_interactive();
+                        printf("\nHave a great day!\n");
                         return EXIT;
                         break;
                 case 'x':
